99e489d78d7f94f7494b4ad582c829d3d5a24a60
Ivan Osipov <extern.osipov_ivan@allianz.com>
2020-10-02 08:08:01 +0300

    init ts configuration

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+/* Types and Variables */
+
+/* Operators
+=
++
+-
+/
+%
+*
+==
+===
+
+* */
+
+/* If else statements */
+
+/* Loops */
+
+/* Functions */
+
+/* Custom types class, interface, type */
+
+/* Also important thinks:
+    1. Errors
+    2. Modules
+ */
